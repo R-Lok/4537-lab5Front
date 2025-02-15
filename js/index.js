bf1eaf8c56097a65b70174d159f9776d5a4113f6
Ryan Lok <rlok.pc@gmail.com>
2025-02-15 15:15:48 -0800

    basic localisation, element setup

---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.getElementById('append').textContent = appendButtonText
+document.getElementById('submit-input').textContent = submitButtonText
+document.getElementById('input-area').placeholder = textareaPlaceholder
---
+++ lang/en/en.js
@@ -0,0 +1,3 @@
+const appendButtonText = "Append Data"
+const submitButtonText = "Submit"
+const textareaPlaceholder = "Type query here..."
