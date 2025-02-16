bbfbde9162a3ce29e5ef673b7cdc133733e0fcf9
Ryan Lok <rlok.pc@gmail.com>
2025-02-15 16:01:35 -0800

    Complete append button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 document.getElementById('append').textContent = appendButtonText
 document.getElementById('submit-input').textContent = submitButtonText
 document.getElementById('input-area').placeholder = textareaPlaceholder
+
+document.getElementById('append').addEventListener('click', async (e)=>{
+    e.preventDefault()
+    const resultBox = document.getElementById('result')
+
+    const requestOptions = {
+        method: "POST",
+        redirect: "follow"
+    };
+
+    try {
+        const response = await fetch("https://www.fortunedgalab.xyz/lab5/button", requestOptions)
+        resultBox.textContent = (await response.json()).status
+    } catch(err) {
+        resultBox.textContent = networkErrorText
+    }
+})
--- lang/en/en.js
+++ lang/en/en.js
@@ -1,3 +1,5 @@
 const appendButtonText = "Append Data"
 const submitButtonText = "Submit"
 const textareaPlaceholder = "Type query here..."
+
+const networkErrorText = "Failed to reach server"
